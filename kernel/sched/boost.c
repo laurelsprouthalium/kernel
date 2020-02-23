cc5c11a2cc10d64b5c8afbfa5adb770f151310ea
derflacco <paul89rulez@gmail.com>
2020-02-23 13:23:11 +0100

    kernel:shed:boost: fox derp

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -53,11 +53,6 @@ static void set_boost_policy(int type)
 		return;
 	}
 
-	if (CONFIG_ARCH_SM6150) {
-		boost_policy = SCHED_BOOST_ON_BIG;
-		return;
-	}
-
 	boost_policy = SCHED_BOOST_ON_ALL;
 }
 
